01ffa9a1175a2c33c4d44960a09fac4f386071a4
Marc Alexandre <github@malexandre.fr>
2020-06-19 18:41:09 +0200

    tool(all): Add react-router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import logo from './logo.svg'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,4 +20,44 @@ function App() {
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/users">
+                  Users
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
