87ff7fe29b919aeea263b04638bbbae2f5ba9de1
Marc Alexandre <github@malexandre.fr>
2020-06-24 18:49:06 +0200

    feat(home): Add navbar

--- src/App.js
+++ src/App.js
@@ -1,62 +1,43 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="btn btn-primary" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+import Ladder from './ladder/Ladder'
+import TournamentList from './tournament/TournamentList'
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users">
-                  Users
-                </Link>
-              </li>
-            </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Classement SFB</a>
+        <button className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#main-navbar"
+          aria-controls="main-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="main-navbar">
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link active" to="/">Ladder <span className="sr-only">(current)</span></Link>
+            <Link className="nav-item nav-link" to="/tournament">Tournament</Link>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-        <Switch>
-          <Route path="/about">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <Home />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/tournament">
+          <div className="container mt-4">
+            <TournamentList />
+          </div>
+        </Route>
+        <Route path="/">
+          <div className="container mt-4">
+            <Ladder />
+          </div>
+        </Route>
+      </Switch>
     </Router>
   )
 }
