dc5bc7ccffe93e4403770d07e090b86b14494215
Marc Alexandre <github@malexandre.fr>
2020-06-27 11:52:30 +0200

    feat(rules): Add missing rules

--- src/rulebook/Rulebook.js
+++ src/rulebook/Rulebook.js
@@ -58,6 +58,11 @@ export default function Rulebook() {
         À la fin de chaque saison, des discussions seront entamées avec la communauté pour décider si des changements
         sont à apporter pour la saison suivante, que ce soit en terme d&apos;organisation ou de règls durant les matchs.
       </p>
+      <p>
+        En plus des saisons, des tournois ponctuels pourront être organisés par la communauté, par exemple pour occuper
+        ceux qui ne sont pas qualifiés pour la phase finale pendant qu&aspos;elle se déroule, ou pour tester
+        d&aspos;autres formats.
+      </p>
 
       <h2>Structure des matchs</h2>
       <p>
@@ -88,6 +93,138 @@ export default function Rulebook() {
         Concernant les phases finales, seul le premier match utilise le tirage au sort pour déterminer le premier
         joueur. Pour les matchs suivants, le premier joueur est le perdant du match précédent.
       </p>
+
+      <h2>Fonctionnement du classement SFB</h2>
+      <p>
+        Inspiré par le classement ATP, chaque participation à une saison ou un tournoi ponctuel peut apporter des points
+        pour le classement SFB. L&apos;équipe de modération sera en charge de décider si un tournoi ponctuel est assez
+        compétitif pour être pris en compte dans le classement. Par exemple, un format qui distribue aléatoirement 3
+        champions à chaque joueur serait un poil trop aléatoire pour être vraiment compétitif.
+      </p>
+      <p>
+        Plus les compétitions sont longues, plus elles peuvent rapporter de points. Les saisons rapportent plus de
+        points que les tournois ponctuels.
+      </p>
+      <p>
+        Seuls ceux présents dans la première moitié du tableau remportent des points par rapport à leur classement
+        final, mais tout le monde remporte des points de participation : 15 points par saison, ou 1 point par match de
+        tournoi ponctuel (avec un maximum de 10 points).
+      </p>
+      <p>
+        Le classement ne prend en compte que les deux dernières saisons (qu&apos;on appellera tournois majeurs), et les
+        deux meilleurs résultats de tournois ponctuels (appelés tournois mineurs) sur la période de ces deux saisons.
+      </p>
+      <p>Voici les points gagner par rapport à la position finale :</p>
+      <table className="table table-striped">
+        <thead className="bg-primary text-light">
+          <tr>
+            <th scope="col">Position finale</th>
+            <th scope="col" colSpan="2">Tournoi majeur</th>
+            <th scope="col" colSpan="2">Tournoi mineur</th>
+          </tr>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Seul(e)</th>
+            <th scope="col">À égalité</th>
+            <th scope="col">Seul(e)</th>
+            <th scope="col">À égalité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td colSpan="2">50</td>
+            <td colSpan="2">35</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td colSpan="2">40</td>
+            <td colSpan="2">25</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>25</td>
+            <td>30</td>
+            <td>15</td>
+            <td>20</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>20</td>
+            <td>25</td>
+            <td>10</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>15</td>
+            <td>19</td>
+            <td>5</td>
+            <td>9</td>
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>10</td>
+            <td>13</td>
+            <td>4</td>
+            <td>7</td>
+          </tr>
+          <tr>
+            <th scope="row">7</th>
+            <td>9</td>
+            <td>11</td>
+            <td>3</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th scope="row">8</th>
+            <td>8</td>
+            <td>10</td>
+            <td>2</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th scope="row">9</th>
+            <td>7</td>
+            <td>9</td>
+            <td>2</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th scope="row">10</th>
+            <td>6</td>
+            <td>8</td>
+            <td>2</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th scope="row">11</th>
+            <td>5</td>
+            <td>6</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th scope="row">12</th>
+            <td>4</td>
+            <td>5</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th scope="row">13 jusqu&apos;à 16</th>
+            <td>3</td>
+            <td>4</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th scope="row">17 et après</th>
+            <td colSpan="2">3</td>
+            <td colSpan="2">2</td>
+          </tr>
+        </tbody>
+      </table>
 
       <h2>Liens importants</h2>
       <dl className="row">
