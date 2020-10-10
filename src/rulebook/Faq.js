6ac0b4a2270ff2d6c0c2e90291c3478108a4f092
Marc Alexandre <github@malexandre.fr>
2020-10-10 15:17:50 +0200

    Update the FAQ with new questions and more French keywords

--- src/rulebook/Faq.js
+++ src/rulebook/Faq.js
@@ -10,59 +10,59 @@ export default function Faq() {
         <dd>Non, il est obligatoire.</dd>
 
         <dt>
-          Si un champion sur lequel a été joué une réaction est mis &quot;Out of Action&quot;, est-ce que les effets de
+          Si un champion sur lequel a été joué une réaction est mis &quot;Hors de Combat&quot;, est-ce que les effets de
           la réaction s&apos;applique toujours autour de l&apos;endroit où il est mort ?
         </dt>
         <dd>
-          Non. Dans la séquence de combat du livre de règle, si le champion est mis &quot;Out of Action&quot; lors de
+          Non. Dans la séquence de combat du livre de règle, si le champion est mis &quot;Hors de Combat&quot; lors de
           l&apos;étape 7, ça arrive bien avant la réaction, donc le champion n&apos;est déjà plus sur plateau lorsque la
           réaction se déroule.
         </dd>
 
         <dt>
-          Si une attaque possède un Push ou un Pull, et que le champion ciblé joue une réaction qui lui permet de se
+          Si une attaque possède une Poussée ou une Attraction, et que le champion ciblé joue une réaction qui lui permet de se
           déplacer, et qu&apos;il se déplace de façon à ne plus être aligné avec l&apos;attaquant, est-ce qu&apos;on
-          fait toujours le Push ou le Pull ?
+          fait toujours la Poussée ou l&apos;Attraction ?
         </dt>
         <dd>
-          Non, on résoud la séquence d&apos;attaque dans l&apos;order. Le Push/Pull étant un effet Post-Attaque, il est
+          Non, on résoud la séquence d&apos;attaque dans l&apos;ordre. La Poussée/Attraction étant un effet Post-Attaque, il est
           résolu après la réaction, et à ce moment là les champions ne sont plus alignés.
         </dd>
 
         <dt>
-          Est-ce que Mariusz, en devenant exalté, gagne Vol de vie directement sur l&apos;attaque qu&apos;il vient
-          d&apos;utiliser s&apos;il est Bloodied ?
+          Est-ce que Mariusz, en devenant exalté, gagne Drain de vie directement sur l&apos;attaque qu&apos;il vient
+          d&apos;utiliser s&apos;il est Mal en Point ?
         </dt>
         <dd>
           Non, les dommages ont été distribué lorsqu&apos;il n&apos;était pas exalté, donc l&apos;attaque n&apos;avait
-          pas Vol de vie à ce moment là.
+          pas Drain de vie à ce moment là.
         </dd>
 
         <dt>
-          Quand une unité subit un fear, peut-elle choisir une direction qui ne permet pas de faire le Dash au maximum
+          Quand une unité subit une Peur, peut-elle choisir une direction qui ne permet pas de faire le Dash au maximum
           quand une qui le permet existe ?
         </dt>
         <dd>
-          S&apos;il y a une direciton qui permet de Dash 2 en s&apos;éloignant de la source du Fear il faut la choisir
-          en priorité devant les direcitons qui ne permettent que de Dash 1.
+          S&apos;il y a une direciton qui permet de Dash 2 en s&apos;éloignant de la source de la Peur il faut la choisir
+          en priorité devant les directions qui ne permettent que de Dash 1.
         </dd>
 
         <dt>
-          Avec la carte Nightmare de Nevamor, s&apos;il traverese plusieurs ennemis, dans quel ordre sont fait les Fear
+          Avec la carte Cauchemar de Nevamor, s&apos;il traverese plusieurs ennemis, dans quel ordre sont fait les Peurs
           ?
         </dt>
         <dd>
-          Le joueur qui subit le Fear choisi dans quel ordre les champions se déplacent. Donc un champion peut se
+          Le joueur qui subit les Peurs choisi dans quel ordre les champions se déplacent. Donc un champion peut se
           déplacer et laisser une place à un autre.
         </dd>
 
         <dt>
-          Si une réaction tue l&apos;attaquant, qui level up ? Le champion attaqué ou le champion à qui appartient la
+          Si une réaction tue l&apos;attaquant, qui devient expérimenté ? Le champion attaqué ou le champion à qui appartient la
           réaction ?
         </dt>
         <dd>Le champion attaqué, sauf si spécifié autrement sur la réaction.</dd>
 
-        <dt>Est-ce qu&apos;un champion peut level up alors qu&apos;il est Out of Action ?</dt>
+        <dt>Est-ce qu&apos;un champion peut devenir expérimenté alors qu&apos;il est Hors de Combat ?</dt>
         <dd>Non.</dd>
 
         <dt>
@@ -70,8 +70,22 @@ export default function Faq() {
           est-ce que l&apos;attaque ne draine que le nombre de points de vie restants, ou les dommages totaux ?
         </dt>
         <dd>
-          Dommages totaux. Donc une attaque 5 lifesteal contre une cible avec 2 points de vie et 0 d&apos;armure soigne
+          Dommages totaux. Donc une attaque 5 Drain de vie contre une cible avec 2 points de vie et 0 d&apos;armure soigne
           l&apos;attaquant de 5.
+        </dd>
+    
+        <dt>
+          Sur la carte Larmes Curatives d&apos;Akhet, de combien je soigne mon allié si je défause une carte Bleue ?
+        </dt>
+        <dd>
+          De 2. Le soin fait référence au Soin 2 de la carte.
+        </dd>
+    
+        <dt>
+          Sur la carte Matraquage de Taze, que veut dire Choc ?
+        </dt>
+        <dd>
+          C&apos;est une erreur d'impression. Il faut remplacer Choc par Étourdissement.
         </dd>
       </dl>
     </div>
